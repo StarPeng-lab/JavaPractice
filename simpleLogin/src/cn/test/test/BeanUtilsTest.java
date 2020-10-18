24e7f41a86d80e6d88a083af4bc0956372ea03cb
pengruolan <pengruolan@prl.com>
2020-10-19 01:39:56 +0800

    '使用BeanUtils工具类简化数据的封装'

---
+++ simpleLogin/src/cn/test/test/BeanUtilsTest.java
@@ -0,0 +1,32 @@
+package cn.test.test;
+
+import org.apache.commons.beanutils.BeanUtils;
+import org.junit.Test;
+import cn.test.domain.User;
+
+import java.lang.reflect.InvocationTargetException;
+
+public class BeanUtilsTest {
+    @Test
+    public void test(){
+
+        User user = new User();
+
+        try {
+            //注意此处操作的是username属性值，不是User类中的成员变量username，而是从set/get方法名中提取出的属性值
+            //即如果User类中成员变量名为username，而set/get方法为setHehe()/getHehe()，则此时user对象为null
+            BeanUtils.setProperty(user,"username","zhangsan");
+            System.out.println(user);
+
+            //此处操作的也是属性值username
+            String name = BeanUtils.getProperty(user,"username");
+            System.out.println(name);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- simpleLogin/src/cn/test/web/servlet/LoginServlet.java
+++ simpleLogin/src/cn/test/web/servlet/LoginServlet.java
@@ -6,9 +6,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
 
 import cn.test.dao.UserDao;
 import cn.test.domain.User;
+import org.apache.commons.beanutils.BeanUtils;
 
 @WebServlet("/loginServlet")
 public class LoginServlet extends HttpServlet {
@@ -16,13 +19,28 @@ public class LoginServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         //1、设置编码
         req.setCharacterEncoding("utf-8");
-        //2、获取请求参数
+
+       /* //2、获取请求参数
         String username = req.getParameter("username");
         String password = req.getParameter("password");
         //3、封装User对象
         User loginUser = new User();
         loginUser.setUsername(username);
-        loginUser.setPassword(password);
+        loginUser.setPassword(password);*/
+
+        //2、获取所有请求参数
+        Map<String, String[]> map = req.getParameterMap();
+        //3.1、创建User对象
+        User loginUser = new User();
+        //3.2、使用BeanUtils，将map集合中的键值对信息（属性名-属性值）封装到对应的JavaBean对象（User对象）中
+        try {
+            BeanUtils.populate(loginUser,map);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+
         //4、调用UserDao的login方法
         UserDao dao = new UserDao();
         User user = dao.login(loginUser);
