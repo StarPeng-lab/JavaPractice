45724ecd41a8682464419a7eba6e8214b69b6aa4
pengruolan <pengruolan@prl.com>
2020-12-03 21:23:05 +0800

    'aop:基于xml配置'

--- SpringAOP/src/main/java/cn/com/aop/Target.java
+++ SpringAOP/src/main/java/cn/com/aop/Target.java
@@ -3,6 +3,7 @@ package cn.com.aop;
 public class Target implements TargetInterface {
     @Override
     public void save() {
+        int i = 2/0;
         System.out.println("target......");
     }
 }
