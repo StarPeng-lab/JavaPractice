07a218aec2774830850881f2d0202c261f195919
pengruolan <pengruolan@prl.com>
2020-12-03 23:09:07 +0800

    '对象交由spring'

--- Spring_Jdbc/src/main/java/jdbc/JdbcTemplateTest.java
+++ Spring_Jdbc/src/main/java/jdbc/JdbcTemplateTest.java
@@ -2,13 +2,29 @@ package jdbc;
 
 import org.junit.Test;
 import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.MessageSourceResolvable;
+import org.springframework.context.NoSuchMessageException;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.ResolvableType;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.Resource;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.beans.PropertyVetoException;
+import java.io.IOException;
+import java.lang.annotation.Annotation;
+import java.util.Locale;
+import java.util.Map;
 
 public class JdbcTemplateTest {
     @Test
-    public void test() throws PropertyVetoException {
+    public void test1() throws PropertyVetoException {
         //通过c3p0连接池，创建数据源对象
         ComboPooledDataSource dataSource = new ComboPooledDataSource();
         dataSource.setDriverClass("com.mysql.jdbc.Driver");
@@ -22,6 +38,14 @@ public class JdbcTemplateTest {
         template.setDataSource(dataSource);
         //3、执行操作
         int row = template.update("insert into u values(?,?)", "zhangsan", 15);
+        System.out.println(row);
+    }
+
+    @Test
+    public void test2() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        JdbcTemplate jdbcTemplate = (JdbcTemplate)context.getBean("jdbcTemplate");
+        int row = jdbcTemplate.update("insert into u values(?,?)", "lisi", 16);
         System.out.println(row);
     }
 }
