8775b3bc765fa60ed35e0d4c728a5d807e522dc4
pengruolan <pengruolan@prl.com>
2021-03-09 02:28:44 +0800

    TCP通信-客户端数据来自文件并接收Server反馈

---
+++ Network/src/TCP_demo4/ClientDemo.java
@@ -0,0 +1,34 @@
+package TCP_demo4;
+
+import java.io.*;
+import java.net.Socket;
+
+/*
+    客户端：数据来自文本文件，接收服务器反馈
+ */
+public class ClientDemo {
+    public static void main(String[] args) throws IOException {
+        Socket s = new Socket("192.168.2.185",1662);
+
+        BufferedReader br = new BufferedReader(new FileReader("Networt\\test.txt"));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(s.getOutputStream()));
+
+        String line;
+        while((line = br.readLine()) != null){
+            bw.write(line);
+            bw.newLine();
+            bw.flush();
+        }
+
+        s.shutdownOutput(); //发出输出结束标记，表示输出结束（避免出现服务器程序一直等待而给不出反馈的问题，因为读数据的方法是阻塞式的）
+
+        //接收服务器反馈
+        BufferedReader brServer = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        String data = brServer.readLine();
+        System.out.println("服务器的反馈：" + data);
+
+        br.close();
+        s.close();
+
+    }
+}
