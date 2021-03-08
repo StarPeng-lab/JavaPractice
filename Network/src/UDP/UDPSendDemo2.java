daf90b8964aac87a6a689261614278d0883d22a8
pengruolan <pengruolan@prl.com>
2021-03-09 00:53:49 +0800

    UDP通信-发送端

--- Network/src/UDP/UDPSendDemo2.java
+++ Network/src/UDP/UDPSendDemo2.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.*;
 
-//UDP发送数据：通过键盘录入数据，直到输入886，停止发送
+//UDP发送数据：通过键盘录入数据，直到输入886，发送数据结束
 public class UDPSendDemo2 {
     public static void main(String[] args) throws IOException {
         //1、创建发送端的Socket对象
