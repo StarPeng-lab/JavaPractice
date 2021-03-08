9671c7ec33e46c57893ff56d881453efe0e02371
pengruolan <pengruolan@prl.com>
2021-03-09 01:06:22 +0800

    UDP通信-发送端

--- Network/src/UDP/UDPSendDemo2.java
+++ Network/src/UDP/UDPSendDemo2.java
@@ -6,6 +6,8 @@ import java.io.InputStreamReader;
 import java.net.*;
 
 //UDP发送数据：通过键盘录入数据，直到输入886，发送数据结束
+//可以开启多个UDPSendDemo2.java并行进程，来发送数据在UDPReceiveDemo2.java的控制台显示，模拟简单版多人聊天
+//如果开启不了，则配置【Edit Configurations】中勾选【Allow parallel run】（允许并行运行），即可
 public class UDPSendDemo2 {
     public static void main(String[] args) throws IOException {
         //1、创建发送端的Socket对象
